246b9db056e54ad564dcf66778c86122e3adbfdb
jrgleason <jackiegleason@gmail.com>
2021-02-03 22:53:48 -0500

    Making a simple deno app to try out

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,11 +1,20 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-
+import { format } from "https://deno.land/x/std/datetime/mod.ts";
 const port = Deno.env.get("PORT") || 8000;
+const secure = false;
 const app = new Application();
 const router = new Router();
-router.get("/deno", ctx =>{
-	ctx.response.body = "This is deno";
+router.get("/time", ctx =>{
+	ctx.response.body = format(new Date(), "MM-dd-yyyy HH:mm");
 })
 app.use(router.routes());
 app.use(router.allowedMethods());
-await app.listen({ port });
+app.addEventListener("listen", str => {
+	console.log(
+		`Listening on: ${str.secure ? "https://" : "http://"}${str.hostname ??
+		"localhost"}:${str.port}`,
+	);
+});
+await app.listen({
+	port, secure
+});
